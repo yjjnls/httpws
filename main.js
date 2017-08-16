093b9a6fe271d5240c3f9e3ffbfa39b590fa7834
zhangmingyi <zhangmingyi@kedacom.com>
2017-08-16 14:37:00 +0800

    request in server basicly ok

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 httpws = require('./lib/http')
 
 httpws.createServer( function( req, res){
+    console.log('got request !');
+    req.on('data',function (data) {
+        console.log('receive data\n',data);
+    });
+    req.on('end',function(){
+        console.log('end');
+
+    });
 
 }).listen(8080);
