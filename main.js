7c36ec28c3d671c65319c5d7e3e70483887cf302
yuanjunjie <yuanjunjie@kedacom.com>
2017-08-16 15:50:25 +0800

    update send msg

--- main.js
+++ main.js
@@ -2,12 +2,25 @@ httpws = require('./lib/http')
 
 httpws.createServer( function( req, res){
     console.log('got request !');
+    console.log(req.headers);
+    console.log(req.url);
     req.on('data',function (data) {
         console.log('receive data\n',data);
     });
     req.on('end',function(){
         console.log('end');
-
+        res.writeHead(200, {
+            "Content-Type": "text/html"
+        });
+        res.write("<html>");
+        res.write("<head>");
+        res.write("<title>Hello World Page</title>");
+        res.write("</head>");
+        res.write("<body>");
+        res.write("Hello World!");
+        res.write("</body>");
+        res.write("</html>");
+        res.end();
     });
 
 }).listen(8080);
