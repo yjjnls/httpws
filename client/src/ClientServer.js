d3da5057bd89fcad9f9df63e7363e744cefffe64
ganchao <ganchao@kedacom.com>
2017-08-21 10:33:56 +0800

    structure adjustment

--- client/src/ClientServer.js
+++
@@ -1,9 +0,0 @@
-
-class ClientServer extends EventEmitter {
-    constructor(requestListener) {
-        super();
-        if (requestListener) {
-            this.on('request', requestListener);
-        }
-    }
-}
